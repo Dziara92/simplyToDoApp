1a24d390d149f235fdeeeb15090a726db1315464
Mateusz Dziura <mateusz.dziura92@gmail.com>
2023-03-09 11:22:20 +0100

    add functionality delete for all array task

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const addTaskBtn = document.querySelector(".addTaskBtn");
 const input = document.querySelector("input");
 const ulToDoList = document.querySelector(".todo-list");
+const warrning = document.querySelector(".worning");
+const clearAllTasks = document.querySelector(".trash");
+const deleteAllP = document.querySelector(".deleteAll");
 let tasks = [];
 
 //add Event Listeners
@@ -14,24 +17,25 @@ function addToDoTask(e) {
   const taskValue = input.value;
 
   if (!taskValue) {
-    alert("please fill task");
+    warrning.classList.add("showworning");
     return;
+  } else {
+    warrning.classList.remove("showworning");
+    let randomIndex = Math.floor(Math.random() * 100);
+    tasks.push({
+      index: randomIndex,
+      taskName: taskValue,
+    });
+    input.value = "";
+    displayTasks();
   }
-  let randomIndex = Math.floor(Math.random() * 100);
-
-  tasks.push({
-    index: randomIndex,
-    taskName: taskValue,
-  });
-  input.value = "";
-  displayTasks();
 }
 
 const displayTasks = () => {
   const tasksToDisplay = tasks.map((task) => {
     return ` <div class="task" id="${task.index}">
         <li>${task.taskName}</li>
-        <button class="checkedBtn">checked</button>
+        <button class="checkedBtn"><i class="fa-solid fa-check"></i></button>
         <button class="deleteBtn" onclick="deleteTask(${task.index})">delete</button>
     </div>`;
   });
@@ -40,13 +44,15 @@ const displayTasks = () => {
 
   const checkedBtn = document.querySelectorAll(".checkedBtn");
   chcekedTask(checkedBtn);
+  deleteAllTasks();
 };
 
 const chcekedTask = (checkedBtn) => {
   checkedBtn.forEach((btn) =>
     btn.addEventListener("click", (e) => {
       let btnItem = e.target;
-      btnItem.classList.toggle("green");
+      console.log(btnItem);
+      btn.classList.toggle("green");
     })
   );
 };
@@ -55,4 +61,20 @@ const deleteTask = (index) => {
   let newArrayTasks = tasks.filter((task) => task.index !== index);
   tasks = newArrayTasks;
   displayTasks();
+};
+
+const deleteAllTasks = () => {
+  if (tasks.length < 1) {
+    clearAllTasks.classList.remove("showtrash");
+    deleteAllP.classList.remove("showDeleteAll");
+    return;
+  } else {
+    clearAllTasks.classList.add("showtrash");
+    deleteAllP.classList.add("showDeleteAll");
+    clearAllTasks.addEventListener("click", () => {
+      clearAllTasks.classList.remove("showtrash");
+      tasks = [];
+      displayTasks();
+    });
+  }
 };
